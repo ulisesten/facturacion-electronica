9da1d40a77509c85bd49835309db736d9ae5181a
ulisesten <ulises.mtz.el@gamil.com>
2021-08-17 18:09:41 -0500

    deleting unused files

--- external/lib/libxslt-lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/usr/local/lib"
-XSLT_LIBS="-lxslt -lxml2"
-XSLT_PRIVATE_LIBS="-lm"
-XSLT_INCLUDEDIR="-I/usr/local/include"
-MODULE_VERSION="xslt-1.1.34"
